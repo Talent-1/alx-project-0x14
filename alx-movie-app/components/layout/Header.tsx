7d79fc4ecd786f377670aef687c6ef879c43c10a
Talent-1 <itzmhiztatalent@gmail.com>
2025-12-08 00:56:59 +0100

    pages

--- alx-movie-app/components/layout/Header.tsx
+++ alx-movie-app/components/layout/Header.tsx
@@ -1,30 +1,26 @@
-import React from 'react';
-import Link from 'next/link';
+import Link from "next/link"
+import Button from "../commons/Button"
 
 const Header: React.FC = () => {
   return (
-    <header className="sticky top-0 z-10 bg-gray-900 shadow-md">
-      <div className="container flex items-center justify-between h-16 max-w-6xl px-4 mx-auto">
-        <Link href="/" className="text-2xl font-bold text-blue-400 hover:text-blue-300">
-          ALX Movie App
-        </Link>
-        <nav>
-          <ul className="flex space-x-4">
-            <li>
-              <Link href="/popular" className="text-white hover:text-blue-400">
-                Popular
-              </Link>
-            </li>
-            <li>
-              <Link href="/top-rated" className="text-white hover:text-blue-400">
-                Top Rated
-              </Link>
-            </li>
-          </ul>
+    <header className="h-28 flex items-center bg-[#171D22] px-4 md:px-16 lg:px-44 text-white">
+      <div className="flex items-center justify-between w-full">
+        <h2 className="text-xl md:text-4xl font-semibold">Cine<span className="text-[#E2D609]">Seek</span></h2>
+        <nav className="hidden md:flex flex-1 justify-center space-x-8">
+          <Link href="/" className="hover:text-[#E2D609] px-4 md:px-8 text-xl transition-colors duration-300 font-semibold">Home</Link>
+          <Link href="/movies" className="hover:text-[#E2D609] px-4 md:px-8 text-xl transition-colors duration-300 font-semibold">Movies</Link>
+          <Link href="/contact" className="hover:text-[#E2D609] px-4 md:px-8 text-xl transition-colors duration-300 font-semibold">Contact</Link>
         </nav>
+        <div className="flex md:hidden">
+          <Button title="Sign in" />
+        </div>
+        <div className="hidden md:flex">
+          <Button title="Sign in" />
+        </div>
       </div>
     </header>
-  );
-};
 
-export default Header;
+  )
+}
+
+export default Header
--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-
-interface LayoutProps {
-  children: React.ReactNode;
-}
+import { ComponentProps } from "@/interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <Header />
-      <main className="flex-grow container max-w-6xl mx-auto p-4">
+      <main className="grow container max-w-6xl mx-auto p-4">
         {children}
       </main>
       <Footer />
